d3bfa62179776bb6da084502e4ff194476bdb2fb
nusuke <nusukep@gmail.com>
2021-02-14 23:00:58 +0900

    add husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const index = () => (
   <>
     <h1 className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      Hello, world.
+      Hello, world.test
     </h1>
   </>
 );
